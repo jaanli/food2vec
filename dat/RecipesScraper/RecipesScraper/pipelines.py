fa3ac19213664f7281bc7b84ddf8734f6c58c57c
Brandon M. Burroughs <brandonmburroughs@gmail.com>
2017-02-07 22:03:55 -0500

    Changing output to be scraper specific

--- dat/RecipesScraper/RecipesScraper/pipelines.py
+++ dat/RecipesScraper/RecipesScraper/pipelines.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
-from scrapy.exporters import JsonItemExporter
+from scrapy.exporters import JsonLinesItemExporter
 
 
 class JsonPipeline(object):
     """Save Pipeline output to JSON."""
-    def __init__(self):
-        self.file = open("recipes.json", 'wb')
-        self.exporter = JsonItemExporter(self.file, encoding='utf-8', ensure_ascii=False)
+    def __init__(self, spider_name):
+        self.file = open("output/{}_recipes.json".format(spider_name), 'wb')
+        self.exporter = JsonLinesItemExporter(self.file, encoding='utf-8', ensure_ascii=False)
         self.exporter.start_exporting()
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            spider_name=crawler.spider.name
+        )
 
     def close_spider(self, spider):
         self.exporter.finish_exporting()
