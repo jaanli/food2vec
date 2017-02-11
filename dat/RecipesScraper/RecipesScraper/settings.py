75c5426c90d10f34103a2e7a8b0670184e193c4f
Brandon M. Burroughs <brandonmburroughs@gmail.com>
2017-02-11 16:52:40 -0500

    Avoiding 429s

--- dat/RecipesScraper/RecipesScraper/settings.py
+++ dat/RecipesScraper/RecipesScraper/settings.py
@@ -72,12 +72,12 @@ ITEM_PIPELINES = {
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
 AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-AUTOTHROTTLE_START_DELAY = 2
+AUTOTHROTTLE_START_DELAY = 3
 # The maximum download delay to be set in case of high latencies
 AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-AUTOTHROTTLE_TARGET_CONCURRENCY = 8.0
+AUTOTHROTTLE_TARGET_CONCURRENCY = 4.0
 # Enable showing throttling stats for every response received:
 #AUTOTHROTTLE_DEBUG = False
 
