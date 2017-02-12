870a87c7f26f180c85e21bd6f143c188537c1407
Brandon M. Burroughs <brandonmburroughs@gmail.com>
2017-02-12 13:19:25 -0500

    Changing spiders to use existing schema and schema processor

--- dat/RecipesScraper/RecipesScraper/items.py
+++ dat/RecipesScraper/RecipesScraper/items.py
@@ -1,10 +1,25 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy import Item
+from scrapy.loader.processors import MapCompose
+
+from utils import remove_extra_whitespace
 
 
-class RecipeItem(scrapy.Item):
+class RecipeItem(Item):
     """Info from recipe to be collected."""
-    recipe = scrapy.Field()
-    ingredients = scrapy.Field()
-    tags = scrapy.Field()
-    url = scrapy.Field()
+    recipe_name = scrapy.Field(
+        input_processor=MapCompose(remove_extra_whitespace)
+    )
+    ingredients = scrapy.Field(
+        input_process=MapCompose(remove_extra_whitespace)
+    )
+    tags = scrapy.Field(
+        input_process=MapCompose(remove_extra_whitespace)
+    )
+    url = scrapy.Field(
+        input_process=MapCompose(remove_extra_whitespace)
+    )
+    description = scrapy.Field(
+        input_process=MapCompose(remove_extra_whitespace)
+    )
--- dat/RecipesScraper/RecipesScraper/spiders/allrecipes_spider.py
+++ dat/RecipesScraper/RecipesScraper/spiders/allrecipes_spider.py
@@ -1,8 +1,12 @@
 """
 Scrape data from allrecipes.com
 """
-import re
 import scrapy
+from scrapy.linkextractors import LinkExtractor
+from extruct.w3cmicrodata import MicrodataExtractor
+from extruct.jsonld import JsonLdExtractor
+from time import sleep
+
 from RecipesScraper.items import RecipeItem
 
 
@@ -10,46 +14,46 @@ class AllRecipesSpider(scrapy.Spider):
     """Spider to scrape All Recipes (allrecipes.com)"""
     name = "allrecipes"
 
-    with open("sitemap/seed_lists/allrecipes_seed_list.txt") as seedfile:
-        start_urls = ["{}?page=1".format(url.strip()) for url in seedfile.readlines()]
+    start_urls = ["http://allrecipes.com/recipes/?page={}".format(page) for page in range(3000)]
 
     def parse(self, response):
-        """Parse recipe page."""
-        recipes = response.css('article.grid-col--fixed-tiles')
+        """Parse the recipe list."""
+        recipes = LinkExtractor(allow="/recipe/\d+/.*").extract_links(response)
         if len(recipes) > 0:
-            for recipe in recipes:
-                try:
-                    recipe_href = response.urljoin(recipe.css('a::attr(href)').extract_first())
-                    if 'video' not in recipe_href and recipe_href != response.url:
-                        yield scrapy.Request(recipe_href, callback=self.parse_recipe)
-                except AttributeError:
-                    self.log("Skipped empty article")
+            for recipe_link in recipes:
+                yield scrapy.Request(recipe_link.url, callback=self.parse_item)
 
-            base_url, page_info = response.url.split("?")
-            page_number = int(page_info.split("=")[1])
-            next_page = "{}?page={}".format(base_url, page_number + 1)
-            yield scrapy.Request(next_page, callback=self.parse)
-
-    def parse_recipe(self, response):
+    def parse_item(self, response):
         """Parse the recipe to get title and ingredients."""
-        recipe_name = self.remove_non_ascii(response.css("h1.recipe-summary__h1::text").extract_first())
-        ingredients = filter(lambda ingredient: ingredient != u'Add all ingredients to list',
-                             response.css("span.recipe-ingred_txt::text").extract())
-        recipe_tags = [li.css("span::text").extract_first().strip() for li in response.css("ul.breadcrumbs li")]
-        #recipe_tags = all_tags[(all_tags.index("World Cuisine") + 1):]
-        self.log("Scraped {}".format(recipe_name))
+        schema_type = "mde"
+        mde = MicrodataExtractor()
+        data = mde.extract(response.body)
+        if len(data['items']) == 0:
+            jslde = JsonLdExtractor()
+            data = jslde.extract(response.body)
+            schema_type = "jsonld"
 
-        recipe = RecipeItem()
-        recipe['recipe'] = recipe_name
-        recipe['ingredients'] = ingredients
-        recipe['tags'] = recipe_tags
+        if schema_type == "mde":
+            recipe = data['items'][0]['properties']
+            recipe_output_item = RecipeItem()
+            recipe_output_item['recipe_name'] = recipe['name']
+            recipe_output_item['ingredients'] = [ingredient for ingredient in recipe['ingredients'] if ingredient not in ['', 'Add all ingredients to list']]
+            recipe_output_item['tags'] = [tag['properties']['title'] for tag in data['items'][1:]]
+            try:
+                recipe_output_item['description'] = recipe['description']
+            except KeyError:
+                recipe_output_item['description'] = None
+            recipe_output_item['url'] = recipe['url']
+        elif schema_type == "jsonld":
+            recipe = data['items'][0]
+            recipe_output_item = RecipeItem()
+            recipe_output_item['recipe_name'] = recipe['name']
+            recipe_output_item['ingredients'] = recipe['ingredients']
+            recipe_output_item['tags'] = [tag['properties']['title'] for tag in data['items'][1:]]
+            try:
+                recipe_output_item['description'] = recipe['description']
+            except KeyError:
+                recipe_output_item['description'] = None
+            recipe_output_item['url'] = recipe['url']
 
-        yield recipe
-
-    @staticmethod
-    def remove_non_ascii(text):
-        """Remove the non ascii characters."""
-        if text is None:
-            return None
-        else:
-            return ''.join([i if ord(i) < 128 else ' ' for i in text])
+        yield recipe_output_item
