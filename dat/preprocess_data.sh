0c7d18c627421b555bde3e1ff593379c3f291e25
Yu Feng <rainwoodman@gmail.com>
2017-03-07 09:19:13 -0800

    improving posix compatibility a bit.
    This file didn't run when @luplatina tried on his vanilla macbook with anaconda 3.

--- dat/preprocess_data.sh
+++ dat/preprocess_data.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
 # concatenate all datasets
-cp srep00196-s3.csv all-recipes.csv
+touch all-recipes.csv
+cat srep00196-s3.csv >> all-recipes.csv
 cat kaggle_recipes.csv >> all-recipes.csv
 cat allrecipes_recipes.csv >> all-recipes.csv
 cat jamieoliver_recipes.csv >> all-recipes.csv
 
 # remove first four lines
-tail -n+4 all-recipes.csv > recipes
+tail -n+4 all-recipes.csv > recipes.1
 
 # print only second field onward
-gawk -iinplace -F, '{print substr($0, index($0, $2))}' recipes
+awk -F, '{print substr($0, index($0, $2))}' recipes.1 > recipes.2
 
 # convert commas to spaces
-gawk -iinplace -F, '{$1=$1}1' OFS=" " recipes
+awk -F, '{$1=$1}1' OFS=" " recipes.2 > recipes.3
 
 # keep recipes with at least two ingredients
-gawk -iinplace -F" " 'NF>1' recipes
+awk -F" " 'NF>1' recipes.3 > recipes.4
+
+cp recipes.4 recipes
 
 
