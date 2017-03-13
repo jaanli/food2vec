fe146904ec6357ee1edf3de3427bc20f3fa67956
Jaan Altosaar <jaan.altosaar@gmail.com>
2017-03-13 17:13:31 -0400

    separate out data and script to replicate blog post experiment

--- src/blog_experiment.sh
+++ src/blog_experiment.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# create the training set
+./process_kaggle_nature.sh
+
 # get the sentence_word2vec module and compile the custom ops
 git submodule update --init
 cd sentence_word2vec
@@ -7,8 +10,9 @@ git submodule update --init
 ./compile_ops.sh
 
 # run word2vec with the recipes as context
+cd sentence_word2vec
 python word2vec_optimized.py \
-  --train_data ../../dat/processed/kaggle_and_nature_recipes_train \
+  --train_data ../../dat/processed/kaggle_and_nature_train \
   --interactive \
   --save_path /tmp \
   --subsample 0 \
