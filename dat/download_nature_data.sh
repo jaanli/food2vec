41508b3d248c693145b06f1a072833735dd5642b
Jaan Altosaar <jaan.altosaar@gmail.com>
2017-03-13 14:46:46 -0400

    rename

--- dat/download_nature_data.sh
+++ dat/download_nature_data.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+
 wget http://www.nature.com/article-assets/npg/srep/2011/111215/srep00196/extref/srep00196-s3.zip
 unzip srep00196-s3.zip
+
+tail -n+5 srep00196-s3.csv > processed/nature_data.csv
+
+rm srep00196-s3.*
+
